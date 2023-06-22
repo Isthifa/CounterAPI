a7db7db1d8de55b2e8a86e9bc172008eb8dfa44c
Isthifa <87200953+Isthifa@users.noreply.github.com>
2023-06-22 14:59:48 +0530

    Service imple

---
+++ Service.java
@@ -0,0 +1,9 @@
+    ResTotalUsersDTO fetchTotalUsers();
+
+    ResTotalSellersDTO fetchTotalSellers();
+
+    ResTotalBuyersDTO fetchTotalBuyers();
+
+    VistorsCountDTO getTotalVisitorsCount();
+
+    List<VisitorsLoginOutDTO> fetchAllLoginLogout();
